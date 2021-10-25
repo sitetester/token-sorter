89fc919fda7ef93ca1c5d9d79ad914b0360d2dc5
sitetester <sitetester2010@gmail.com>
2021-10-25 18:15:12 +0300

    refactoring

--- jsonHelper.go
+++ jsonHelper.go
@@ -7,14 +7,14 @@ import (
 
 type JsonHelper struct{}
 
-func (h JsonHelper) ToStruct(text string, token *Token) {
+func (jh *JsonHelper) ToStruct(text string, token *Token) {
 	err := json.Unmarshal([]byte(text), token)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func (h JsonHelper) ToJson(token Token) string {
+func (jh *JsonHelper) ToJson(token Token) string {
 	bytes, err := json.Marshal(token)
 	if err != nil {
 		log.Fatal(err)
