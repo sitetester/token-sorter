092af51b1e147a2049bcfec877c57f77be882902
sitetester <sitetester2010@gmail.com>
2021-10-25 19:27:03 +0300

    refactoring

--- token-sorter.go
+++ token-sorter.go
@@ -6,7 +6,7 @@ const sortByFieldName = "name"
 const sortByFieldAddress = "address"
 
 func main() {
-	input := flag.String("input", "data/data.in", "Input file to sort")
+	input := flag.String("input", "./data/data.in", "Input file to sort")
 	output := flag.String("output", "./data.out", "Output file to store sorted data")
 	// In general, we don’t need to specify buffer, since Golang bufio scanner uses an internal buffer by default
 	bufferSize := flag.Int("buffer-size", 1048576, "buffer size to use for file operations")
